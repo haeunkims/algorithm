ac6c9d2e5ff02dd7d6924222c98b6320b712cfdb
Haeun Kim <gracekim.an@gmail.com>
2021-08-30 16:43:16 +0900

    Bubble sort implementation

--- Udemy/BubbleSort/bubbleSort.js
+++ Udemy/BubbleSort/bubbleSort.js
@@ -1,14 +1,15 @@
 function bubble(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+        swap(arr, j, j + 1);
       }
     }
   }
   return arr;
 }
-console.log(bubble([1, 23, 42, 2]));
-//FIX MONDAY INCOMPLETE!!!
+console.log(bubble([2, 8, 22, 88, 555]));
