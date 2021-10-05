dfcf5bf5d068a0e44ccc1ba3610e0700a890f4b0
Haeun Kim <gracekim.an@gmail.com>
2021-10-05 18:13:57 +0900

    Solved

--- Udemy/Selection Sort/selectionSort.js
+++ Udemy/Selection Sort/selectionSort.js
@@ -1,17 +1,18 @@
-function selectionSort(arr) {
-  let min;
+function sort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < arr[i + 1]) {
-      min = arr[i];
-      if (min !== arr[i]) {
-        const swap = (arr, idx1, idx2) => {
-          [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-        };
+    let min = i;
+    // console.log(arr[min]);
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+        // console.log(arr[min]);
       }
+      if (min !== arr[i]) swap(arr, i, min);
     }
   }
   return arr;
 }
-console.log(selectionSort([12, 1, 3, 5, 2]));
-
-//not finished
+console.log(sort([5, 987, 123, 45, 2, 7, 1]));
