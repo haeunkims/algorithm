c5bb403905d91073093aa13f00d43c45ce462ef2
Haeun Kim <gracekim.an@gmail.com>
2021-11-03 19:06:47 +0900

    pesudo code

---
+++ LeetCode/Longest Common Prefix/LongestComPre.js
@@ -0,0 +1,13 @@
+function longComPre(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    // for(let j = 0; j < )
+    console.log(arr[i]);
+  }
+}
+console.log(longComPre(["flower", "flour", "Fly", "Fax"]));
+//pseudo code
+//loop through each str
+// compare first letter of each string
+// if first letter equal to 0 return " "
+//have a counter to store letter --> counter ++
+// return the largest amount
