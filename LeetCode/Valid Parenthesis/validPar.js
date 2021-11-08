9a3cd6ffb564b09935243ed516b86c8f1c105a1a
Haeun Kim <gracekim.an@gmail.com>
2021-11-08 18:04:45 +0900

    start of algo

---
+++ LeetCode/Valid Parenthesis/validPar.js
@@ -0,0 +1,12 @@
+function validPar(str) {
+  console.log(str);
+}
+console.log(validPar("()[]{}"));
+
+// Algorithm:
+
+// Declare a character stack S.
+// Now traverse the expression string exp.
+// If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
+// If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) then pop from stack and if the popped character is the matching starting bracket then fine else brackets are not balanced.
+// After complete traversal, if there is some starting bracket left in stack then “not balanced”
